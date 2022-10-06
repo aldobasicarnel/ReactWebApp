5f19853c2717aa6b1be40d2a090f8fc2fd89fbca
aldobasicarnel <arnelaldobasic8@gmail.com>
2022-10-06 14:24:42 +0200

    Contact page

--- react-web-shop/src/Components/ContactPage/Contact.js
+++ react-web-shop/src/Components/ContactPage/Contact.js
@@ -1,12 +1,12 @@
 import ShopTitle from "../ShopTitle/ShopTItle";
 import ContactForm from "./ContactForm/ContactForm";
-
+import "./Contact.css";
 const ContactPage = () => {
   return (
     <div>
       <ShopTitle title={`Contact`} />
       <div>
-        <h3>Contact</h3>
+        <h3 className="form-title">Contact Us</h3>
         <ContactForm />
       </div>
     </div>
