892254040a2757cf2476ab0244c1a57db20b3e2c
aldobasicarnel <arnelaldobasic8@gmail.com>
2022-10-11 13:19:55 +0200

    Checkout page done

--- react-web-shop/src/Components/CheckoutPage/Checkout/Checkout.js
+++ react-web-shop/src/Components/CheckoutPage/Checkout/Checkout.js
@@ -4,13 +4,14 @@ import img from "../../HomePage/Slider/images/jeans.png";
 const Checkout = () => {
   return (
     <div className="flexbox-container">
-      <div className="checkout-title">
-        <h5>Items</h5>
-        <h5>Name</h5>
-        <h5>Price</h5>
-        <h5>Quantity</h5>
-      </div>
       <div className="flexbox">
+        {" "}
+        <div className="checkout-title">
+          <h5>Items</h5>
+          <h5>Name</h5>
+          <h5>Price</h5>
+          <h5>Quantity</h5>
+        </div>
         <div className="product-info">
           <button className="delete-btn">X</button>
           <img className="info-img" src={img} alt=""></img>
@@ -25,18 +26,21 @@ const Checkout = () => {
           </div>
         </div>
         <div className="checkout-info">
-          <div className="price-info">
-            <p className="price">Subtotal:</p>
-            <p className="price">$244</p>
+          <div className="order-info">
+            <div className="price-info">
+              <p className="price">Subtotal:</p>
+              <p className="price">$244</p>
+            </div>
+            <div className="shipping-info">
+              <p className="shipping">Shipping:</p>
+              <a href="#">Add Info</a>
+            </div>
+            <div className="total-amount">
+              <p className="amount">Grand Total:</p>
+              <p className="amount">$265</p>
+            </div>
           </div>
-          <div className="shipping-info">
-            <p className="shipping">Shipping:</p>
-            <a href="#">Add Info</a>
-          </div>
-          <div className="total-amount">
-            <p className="amount">Grand Total:</p>
-            <p className="amount">$265</p>
-          </div>
+
           <button className="order-btn">Proceed to checkout</button>
         </div>
       </div>
