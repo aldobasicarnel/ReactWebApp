c176f5ee78056a7146dbaa33b36595d1d6d5dded
aldobasicarnel <arnelaldobasic8@gmail.com>
2022-10-20 14:39:30 +0200

    Completed Product-detail-page

--- react-web-shop/src/Components/ProductDetailPage/ProductDetailItem.js
+++ react-web-shop/src/Components/ProductDetailPage/ProductDetailItem.js
@@ -1,6 +1,10 @@
 import "./ProductDetailItem.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../Store/Cart-Slice";
 
 const ProductDetailItem = (props) => {
+  const dispatch = useDispatch();
+
   const {
     id,
     title,
@@ -11,28 +15,50 @@ const ProductDetailItem = (props) => {
     price,
     stock,
     rating,
+    quantity,
   } = props.products;
+
+  const stockValue = stock > 0 ? `On Stock` : `Out of stock`;
+
+  const addToCartHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        img: thumbnail,
+        name: title,
+        price,
+        quantity,
+      })
+    );
+  };
 
   return (
     <div className="detail-container">
       <div className="detail-image" key={id}>
         <img className="d-img" alt="" src={thumbnail} />
       </div>
-      <div className="detail-info">
-        <div className="details-title">
-          <h1 className="d-title">{title}</h1>
-          <div className="title-desc">
-            <h2 className="d-brand">{brand}</h2>
-            <h2 className="d-category">{category}</h2>
+      <div className="details">
+        <div className="detail-info">
+          <div className="details-title ">
+            <h1 className="d-title">{title}</h1>
+            <div className="title-desc">
+              <h2 className="d-brand">{brand}</h2>
+              <h2 className="d-category">{category}</h2>
+            </div>
+          </div>
+          <div className="details-price">
+            <span className="desc">{description}</span>
+            <div className="price-desc">
+              <span className="d-price">${price}</span>
+              <span className="d-price">{stockValue}</span>
+              <span className="d-price">Rating {rating}</span>
+            </div>
           </div>
         </div>
-        <div className="details-price">
-          <span>{description}</span>
-          <div className="price-desc">
-            <span>${price}</span>
-            <span>On Stock: {stock}</span>
-            <span>Rating {rating}</span>
-          </div>
+        <div className="details-btn">
+          <button className="d-btn" onClick={addToCartHandler}>
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
